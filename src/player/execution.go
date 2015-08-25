15d4943efa4845d79e55186046991a6508f48a3d
Michael Chapman <michael.chapman@anchor.net.au>
2015-08-25 14:54:41 +1000

    os.Process.Wait and friends have changed in Go 1
    https://golang.org/doc/go1#os

--- src/player/execution.go
+++ src/player/execution.go
@@ -8,6 +8,7 @@ import (
 	"bufio"
 	"strings"
 	o "orchestra"
+	"syscall"
 )
 
 func ExecuteTask(task *TaskRequest) <-chan *TaskResponse {
@@ -130,24 +131,25 @@ func doExecution(task *TaskRequest, completionChannel chan<- *TaskResponse) {
 		task.MyResponse.State = RESP_FAILED_HOST_ERROR
 		return
 	}
-	wm, err := proc.Wait(0)
+	wm, err := proc.Wait()
 	if err != nil {
 		o.Warn("job%d: Error waiting for process", task.Id)
 		task.MyResponse.State = RESP_FAILED_UNKNOWN
 		// Worse of all, we don't even know if we succeeded.
 		return
 	}
-	if !(wm.WaitStatus.Signaled() || wm.WaitStatus.Exited()) {
+	ws, _ := wm.Sys().(syscall.WaitStatus)
+	if !(ws.Signaled() || ws.Exited()) {
 		o.Assert("Non Terminal notification received when not expected.")
 		return
 	}
-	if wm.WaitStatus.Signaled() {
+	if ws.Signaled() {
 		o.Warn("job%d: Process got signalled", task.Id)
 		task.MyResponse.State = RESP_FAILED_UNKNOWN
 		return
 	}
-	if wm.WaitStatus.Exited() {
-		if 0 == wm.WaitStatus.ExitStatus() {
+	if ws.Exited() {
+		if 0 == ws.ExitStatus() {
 			o.Warn("job%d: Process exited OK", task.Id)
 			task.MyResponse.State = RESP_FINISHED
 		} else {
